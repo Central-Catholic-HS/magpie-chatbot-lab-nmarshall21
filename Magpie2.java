a9e321d320e65f20b827b2b15a40ea3854198db9
nmarshall21 <33760941+nmarshall21@users.noreply.github.com>
2020-10-16 08:21:43 -0400

    Update Magpie2.java

--- Magpie2.java
+++ Magpie2.java
@@ -42,6 +42,11 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (statement.indexOf("cat") >= 0
+			 	|| statement.indexOf("dog") >= 0
+		{
+			response = "Tell me more about your pets.";
+		}
 		else
 		{
 			response = getRandomResponse();
